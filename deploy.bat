1f81c516f5e7093ba37413111b5376b4c4e40572
Oliver Norred <norredoliver@gmail.com>
2021-03-07 23:47:54 -0800

    oops forgot 404 :)

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 git add .
-git commit -m "some big INTERNAL changes. changed class to id for container elements. rewidthing of blog posts. :)"
+git commit -m "oops forgot 404 :)"
 git pull origin main
 git push origin main
 firebase deploy
