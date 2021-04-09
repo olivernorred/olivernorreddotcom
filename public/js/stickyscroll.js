b005bae7bdd5cc60595bfe9719415cae90c29d6d
Oliver Norred <norredoliver@gmail.com>
2021-04-09 00:53:50 -0700

    changed from transform to scroll

--- public/js/stickyscroll.js
+++ public/js/stickyscroll.js
@@ -1,33 +1,19 @@
-// const workcontainer = document.querySelector("#portfoliosticky")
-// const content = document.querySelector("#content")
-// const foot = document.querySelector("#footer")
-
-// window.addEventListener("load", ()=>{
-// 	content.style.height = `${workcontainer.scrollWidth}px`;
-// })
-
-// window.addEventListener("scroll", ()=> {
-// 	workcontainer.scrollTo(-content.getBoundingClientRect().top, 0);
-// })
-
-
-
-const spaceHolder = document.querySelector('#content');
-const horizontal = document.querySelector('#portfoliogrid');
-spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
+const spaceHolder = document.querySelector('#content')
+const horizontal = document.querySelector('#portfoliogrid')
+spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`
 
 function calcDynamicHeight(ref) {
-  const vw = window.innerWidth;
-  const vh = window.innerHeight;
-  const objectWidth = ref.scrollWidth;
-  return objectWidth - vw + vh + 48; // 150 is the padding (in pixels) desired on the right side of the .cards container. This can be set to whatever your styles dictate
+	const vw = window.innerWidth
+	const vh = window.innerHeight
+	const objectWidth = ref.scrollWidth
+	return objectWidth - vw + vh + 48
 }
 
 window.addEventListener('scroll', () => {
-  const sticky = document.querySelector('#portfoliosticky');
-  horizontal.style.transform = `translateX(-${sticky.offsetTop}px)`;
-});
+	const sticky = document.querySelector('#portfoliosticky')
+	sticky.scrollTo(sticky.offsetTop,0)
+})
 
 window.addEventListener('resize', () => {
-  spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
-});
+	spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`
+})
