0ebe10c53771a44db2b7ffd30feeb717608d9e7f
Oliver Norred <norredoliver@gmail.com>
2023-11-06 11:26:15 -0800

    remove everything

--- public/js/helpingfoods.js
+++
@@ -1,1 +0,0 @@
-const shiftingItems = document.querySelectorAll('.shiftingitem');
--- public/js/scrollvideo.js
+++
@@ -1,5 +0,0 @@
-const video = document.querySelector("#dancingloop")
-window.addEventListener('scroll', ()=>{
-	video.pause();
-	video.currentTime = (window.scrollY/500) % video.duration;
-})
